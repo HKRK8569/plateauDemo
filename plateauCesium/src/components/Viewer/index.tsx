d85b84ca66ca466115c161832d5884b5648bc977
HKRK8569 <kayann3021@gmail.com>
2023-12-18 13:16:52 +0900

    cesiumにplateauの描画を追加

--- plateauCesium/src/components/Viewer/index.tsx
+++ plateauCesium/src/components/Viewer/index.tsx
@@ -1,4 +1,4 @@
-import { Viewer as CesiumViewer, Color, Ion } from "cesium";
+import { Cartesian3, Cesium3DTileset, Viewer as CesiumViewer } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import { useLayoutEffect, useRef, useState } from "react";
 
@@ -7,47 +7,52 @@ export const Viewer = () => {
   const [Viewer, setViewer] = useState<CesiumViewer>();
 
   useLayoutEffect(() => {
-    if (ref.current == null) {
-      setViewer(undefined);
-      return;
-    }
+    (async () => {
+      if (ref.current == null) {
+        setViewer(undefined);
+        return;
+      }
 
-    const viewer = new CesiumViewer(ref.current, {
-      // 今回は使用しないため、すべての組み込みUIを非表示にします。
-      animation: false,
-      baseLayerPicker: false,
-      fullscreenButton: false,
-      geocoder: false,
-      homeButton: false,
-      infoBox: false,
-      sceneModePicker: false,
-      selectionIndicator: false,
-      timeline: false,
-      navigationHelpButton: false,
-      navigationInstructionsInitiallyVisible: false,
-    });
+      const viewer = new CesiumViewer(ref.current, {
+        // 今回は使用しないため、すべての組み込みUIを非表示にします。
+        animation: false,
+        baseLayerPicker: false,
+        fullscreenButton: false,
+        geocoder: false,
+        homeButton: false,
+        infoBox: false,
+        sceneModePicker: false,
+        selectionIndicator: false,
+        timeline: false,
+        navigationHelpButton: false,
+        navigationInstructionsInitiallyVisible: false,
+      });
 
-    // // Viewerのピクセル密度をデバイスのピクセル密度に合わせます。
-    // // 注意：レンダリング品質は向上しますが、GPUへの負荷は大きくなります。
-    viewer.resolutionScale = window.devicePixelRatio;
+      viewer.camera.setView({
+        destination: Cartesian3.fromDegrees(138, 29, 4000000),
+        orientation: {
+          heading: 0, // 水平方向の回転度（ラジアン）
+          pitch: -1.4, // 垂直方向の回転度（ラジアン） 上を見上げたり下を見下ろしたり
+          roll: 0,
+        },
+      });
+      viewer.resolutionScale = window.devicePixelRatio;
+      const scene = viewer.scene;
+      scene.skyBox = undefined as any;
+      scene.globe.depthTestAgainstTerrain = true;
 
-    // // 今回は使用しないため、組み込みのImagery Layerを削除します。
-    // viewer.scene.imageryLayers.removeAll();
+      const tileset = await Cesium3DTileset.fromUrl(
+        import.meta.env.VITE_PLATEAU_URL
+      );
 
-    // // 視覚的なパラメータ調整です。
-    const scene = viewer.scene;
-    scene.skyBox = undefined as any;
+      scene.primitives.add(tileset);
 
-    // // デフォルトでは地形に対してデプステストが行われません（地面にめり込んでいる建物
-    // // やその部分が表示される）。PLATEAUの3D都市モデルを用いる場合には基本的にtrueに
-    // // することになるでしょう。
-    scene.globe.depthTestAgainstTerrain = true;
-
-    setViewer(viewer);
-
-    return () => {
-      viewer.destroy();
-    };
+      viewer.flyTo(tileset);
+      setViewer(viewer);
+      return () => {
+        viewer.destroy();
+      };
+    })();
   }, []);
   return <div ref={ref} className="h-screen w-screen"></div>;
 };
